28312e7160f04991afff89f8d7463a293a1fee38
omerugi <omerugi@users.noreply.github.com>
2022-07-25 13:12:02 +0300

    first

---
+++ lesson_1/main.py
@@ -0,0 +1,21 @@
+def hello(name , age):
+    print(name)
+
+if __name__ == '__main__':
+    my_name = "omer"  # This is a string with my name
+    num = 3463465345  # This is an int
+    num_duble = 30.123  # This is a double
+    print("First task:")
+    name_1 = "harel"
+    age_1 = 12
+    print(name_1, age_1)
+    print("Second task:")
+    name_2 = "ella"
+    age_2 = 12
+    print(name_1, name_2, age_1, age_2)
+    print("Third task:")
+    name_3 = input("what is you'r name")
+    age_3 = input("what is you'r age?")
+    print(name_3, age_3)
+
+

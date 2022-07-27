c8551319a6548c5cdcdb4a014accf152290c1166
omerugi <omerugi@users.noreply.github.com>
2022-07-27 15:15:20 +0300

    update l2

---
+++ lesson_2/hw_2.py
@@ -0,0 +1,52 @@
+if __name__ == '__main__':
+    print("<<< Home Work - Lesson 2 >>>")
+
+    print("First question:")
+    '''
+    What do you think is the -type- of the variable "x" below?
+    **Hint: it would be the same type no matter the input.
+    '''
+    x = input("type something: ")
+    '''
+    (1) str
+    (2) int
+    (3) none
+    '''
+
+    print("Second question:")
+    '''
+    Only after you answered the first question - check the type of "x" and print it.
+    **Hint 1: There is in "main_2.py" a line of code that get the type of a variable. 
+    **Hint 2: It's in the "try - except" code block.
+    '''
+    print()  # Fill in the missing code
+    # Try to play with until you make it! if you get stuck don't - let me know :)
+
+    print("Third question:")
+    '''
+    Write a simple program using everything we learned in lesson 2:
+    Create 2 variables - one with your age, and one with your dad's age.
+        * Example:
+            x = 2
+            y = 3
+    add them together and keep the value in new variable and print this new variable.
+        * Example: 
+            x = 2
+            y = 3
+            new_var = x+y
+            print(new_var)
+     
+    Then create a "try - except" block.
+    In the try block add to "new_var" a variable from different type so we would get an error.
+    Inside the except block print why we got an error.
+        * Example:
+            try:
+                new_var + ??? # Fill in the missing code
+            except:
+                print(why we got an error)
+    Rules:
+    1. Give the variables good names.
+        *Hint: Remember what we did in "main_2.py", check lines 55-56, names that have meaning.
+    2. Use Typing like in "main_2.py" lines 110-113, what is the type of each variable.
+    3. Add comments explaining the code- what do you store in each variable, what are you doing in the "try" code block. 
+    '''

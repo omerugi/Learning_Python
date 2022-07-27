ab66360b18374f8cebec1e2cf90c038aa4d62546
omerugi <omerugi@users.noreply.github.com>
2022-07-27 13:35:50 +0300

    update l2

--- lesson_1/hw_1.py
+++ lesson_1/hw_1.py
@@ -2,3 +2,14 @@ if __name__ == '__main__':
     print("hw-1")  # Input name + age 3 people
 
     print("hw-1")  # 3 foods you like - print each one in new row from less to most fav
+
+    name=input("what is you'r name?")
+    age=input("what is you'r age?")
+    print(age,name)
+    name_1=input("what is you're fav food?")
+    name_2=input("what is you're second fav food?")
+    print(name_1,name_2)
+
+    print("lobster")
+    print("chocolate")
+    print("soup")
